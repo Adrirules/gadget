6adadd996405343e5175bf710e888e445824d65d
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2022-10-27 18:28:19 +0200

    multiple image in form + multiple image in show

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,4 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+
 import opennewtab from "./opennewtab"
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  has_one_attached :photo
   has_one_attached :cover
+  has_many_attached :photos
   belongs_to :user
   has_rich_text :ontent
   acts_as_taggable_on :tags
