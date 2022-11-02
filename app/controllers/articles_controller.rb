95793f27d6b79cde30df7dc2338406e94e25e5ec
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2022-11-02 16:44:48 +0100

    style devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit , :update, :destroy]
+  skip_before_action :authenticate_user!, :only => [:index, :show, :tagged, :categged]
 
 
   def index
