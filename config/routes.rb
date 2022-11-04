01c14f787ad0cd1932de8794ab0d6534bd98ca84
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2022-11-04 15:24:50 +0100

    404 design + nice routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   end
   root to: "articles#index"
   resources :articles, except: :index
-  get '/categged', to: "articles#categged", as: :categged
+  get 'categories/:categ', to: "articles#categged", as: :categged
 
-  get '/tagged', to: "articles#tagged", as: :tagged
+  get 'tags/:tag', to: "articles#tagged", as: :tagged
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
